8ae719d4402654ceb1ffbae03627f52a552b04b1
nokken65 <nokken65182@gmail.com>
2025-12-28 04:59:09 +0300

    refactor: configuration restructuring

---
+++ index.js
@@ -0,0 +1,9 @@
+import configJson from './.prettierrc.json' with { type: 'json' }
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = configJson
+
+export default config
--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-const baseConfig = require('./.prettierrc.json')
-
-module.exports = {
-  ...baseConfig,
-  plugins: [
-    require.resolve('@ianvs/prettier-plugin-sort-imports'),
-  ],
-}

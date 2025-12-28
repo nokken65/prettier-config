e15229ec59a2dc6423001ae275fd72a761fcf696
nokken65 <nokken65182@gmail.com>
2025-12-28 06:51:29 +0300

    fix: all

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import configJson from './.prettierrc.json' with { type: 'json' }
  * @see https://prettier.io/docs/configuration
  * @type {import("prettier").Config}
  */
-const config = { ...configJson }
+const config = {
+  ...configJson
+}
 
 export default config
